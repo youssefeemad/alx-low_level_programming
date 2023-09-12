f424413529e95b30ddb71bbf4eb87a24657450d3
youssef_emad <eyoussef228@gmail.com>
2023-09-12 10:58:23 -0700

    new

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,24 +5,30 @@
  */
 void jack_bauer(void)
 {
-	int h1, h2, m1, m2;
+    int h1, h2, m1, m2;
 
-	for (h1 = 0; h1 <= 2; h1++)
-	{
-		for (h2 = 0; h2 <= 9; h2++)
-		{
-			for (m1 = 0; m1 <= 5; m1++)
-			{
-				for (m2 = 0; m2 <= 9; m2++)
-				{
-					_putchar('0' + h1);
-					_putchar('0' + h2);
-					_putchar(':');
-					_putchar('0' + m1);
-					_putchar('0' + m2);
-					_putchar('\n');
-				}
-			}
-		}
-	}
+    for (h1 = 0; h1 <= 2; h1++)
+    {
+        for (h2 = 0; h2 <= 9; h2++)
+        {
+            // Update the loop condition for minutes to stop at 59
+            for (m1 = 0; m1 <= 5; m1++)
+            {
+                for (m2 = 0; m2 <= 9; m2++)
+                {
+                    if (h1 == 2 && h2 == 4 && m1 == 5 && m2 == 9)
+                    {
+                        // Stop when reaching 23:59
+                        return;
+                    }
+                    _putchar('0' + h1);
+                    _putchar('0' + h2);
+                    _putchar(':');
+                    _putchar('0' + m1);
+                    _putchar('0' + m2);
+                    _putchar('\n');
+                }
+            }
+        }
+    }
 }
