5e149e8a7d47c99a1f9e171528be8178940b2b42
youssef_emad <eyoussef228@gmail.com>
2023-09-12 11:04:27 -0700

    m

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -15,9 +15,10 @@ void jack_bauer(void)
             {
                 for (m2 = 0; m2 <= 9; m2++)
                 {
-                    if (h1 == 2 && h2 == 4 && m1 == 5 && m2 == 9)
+                    if (h1 == 2 && h2 == 3 && m1 == 5 && m2 == 9)
                     {
-			    return;
+                        /* Stop when reaching 23:59 */
+                        return;
                     }
                     _putchar('0' + h1);
                     _putchar('0' + h2);
