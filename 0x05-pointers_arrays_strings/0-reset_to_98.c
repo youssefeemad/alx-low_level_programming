3978814803480d1d2192eeafe5f02807314a0fb0
youssef <eyoussef228@gmail.com>
2023-09-18 07:21:57 +0300

    new

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Updates the value pointed to by n to 98.
+ * @n: A pointer to an integer.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,39 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Function prototypes */
+
+/* Task 0 */
+void reset_to_98(int *n);
+
+/* Task 1 */
+void swap_int(int *a, int *b);
+
+/* Task 2 */
+int _strlen(char *s);
+
+/* Task 3 */
+void _puts(char *str);
+
+/* Task 4 */
+void print_rev(char *s);
+
+/* Task 5 */
+void rev_string(char *s);
+
+/* Task 6 */
+void puts2(char *str);
+
+/* Task 7 */
+void puts_half(char *str);
+
+/* Task 8 */
+void print_array(int *a, int n);
+
+/* Task 9 */
+char *_strcpy(char *dest, char *src);
+
+/* _putchar function (provided separately) */
+int _putchar(char c);
+
+#endif /* MAIN_H */
