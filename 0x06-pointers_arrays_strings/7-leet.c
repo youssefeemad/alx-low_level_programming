14f5655138b6805451c4b9810851d1fea18f600b
youssef_emad <eyoussef228@gmail.com>
2023-09-21 14:03:41 -0700

    new

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -14,9 +14,9 @@ char *leet(char *s)
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for(j = 0; j < 10; j++)
+		for (j = 0; j < 10; j++)
 		{
-			if(s[i] == s1[j])
+			if (s[i] == s1[j])
 			{
 				s[i] = s2[j];
 			}
