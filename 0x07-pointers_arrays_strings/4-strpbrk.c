7db2a018f0411c86069c6acd33ffcbad0708c534
youssef <eyoussef228@gmail.com>
2023-09-25 22:52:34 +0300

    new

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,17 +10,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-    while (*s != '\0')
-    {
-        char *a = accept;
-        while (*a != '\0')
-        {
-            if (*s == *a)
-                return (s);
-            a++;
-        }
-        s++;
-    }
+	while (*s != '\0')
+	{
+		char *a = accept;
+		while (*a != '\0')
+		{
+			if (*s == *a)
+				return (s);
+			a++;
+		}
+		s++;
+	}
 
-    return (NULL);
+	return (NULL);
 }
