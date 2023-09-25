8e4aa0a99ef0e63786808e9ef0b476beff076d90
youssef <eyoussef228@gmail.com>
2023-09-25 23:06:15 +0300

    new

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,26 +1,17 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* Function prototypes */
 char *_memset(char *s, char b, unsigned int n);
-
-/* 1. memcpy */
 char *_memcpy(char *dest, char *src, unsigned int n);
-
-/* 2. strchr */
 char *_strchr(char *s, char c);
-
-/* 3. strspn */
 unsigned int _strspn(char *s, char *accept);
-
-/* 4. strpbrk */
 char *_strpbrk(char *s, char *accept);
-
-/* 5. strstr */
 char *_strstr(char *haystack, char *needle);
-
-/* 6. Chess is mental torture (please provide chessboard content) */
-
-/* 7. The line of life is a ragged diagonal between duty and desire */
+void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 
-#endif
+/* Custom putchar function */
+int _putchar(char c);
+
+#endif /* MAIN_H */
