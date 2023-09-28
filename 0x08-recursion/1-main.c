82b436eda6f6019b1bd024e6188a1c7ae0cc61d5
youssef <eyoussef228@gmail.com>
2023-09-28 17:16:40 +0300

    new

---
+++ 0x08-recursion/1-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * main - Test _print_rev_recursion function.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    _print_rev_recursion("Colton Walker");
+    return (0);
+}
