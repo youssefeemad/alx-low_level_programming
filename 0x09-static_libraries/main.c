0db1aea56e03de34b1dc5e5a5cea5d67a41416af
youssef <eyoussef228@gmail.com>
2023-10-02 17:12:23 +0300

    new

--- 0x09-static_libraries/main.c
+++ 0x09-static_libraries/main.c
@@ -1,5 +1,9 @@
 #include "main.h"
-
+/**
+ * main - prints a quote by Kevin Mitnick
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
 	_puts("\"At the end of the day, my goal was to be the best hacker\"\n"
