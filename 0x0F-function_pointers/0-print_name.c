fffb239b55ebf3c247ed6529c58b7d3932e5724e
youssef <eyoussef228@gmail.com>
2023-10-11 19:28:25 +0300

    new

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -4,12 +4,13 @@
 /**
  * print_name - prints a name
  * @name: name to print
- * @f: function to print name
+ * @f: function to print name with
  *
  * Return: void
  */
 void print_name(char *name, void (*f)(char *))
 {
-	if (f != NULL)
-		f(name);
+	if (f == NULL)
+		return;
+	f(name);
 }
