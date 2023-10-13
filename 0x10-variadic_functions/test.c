968928e17395929d92ce61f31aaaa07aa7e6d326
youssef <eyoussef228@gmail.com>
2023-10-13 08:13:17 +0300

    new

--- 0x10-variadic_functions/test.c
+++ 0x10-variadic_functions/test.c
@@ -1,31 +1,27 @@
 #include <stdio.h>
 #include <stdarg.h>
 
-int sum_them_all(const unsigned int n, ...)
+void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list args;
-	int i, sum = 0;
+	int i = n;
+	va_list ar;
 
-	if (n == 0)
+	if (!n)
 	{
-		return (0);
+		printf ("\n");
+		return;
 	}
-	va_start(args, n);
-	for (i = 0; i < n; i++)
+	va_start(ar,n);
+	for (i = 0 ; i < n ; i++)
 	{
-		sum += va_arg(args, int);
+		printf ("%d%s",va_arg(ar ,int), 
+		(i < n - 1) ? (separator ? separator : "") : "\n");
 	}
-	va_end(args);
-	return (sum);
+	va_end(ar);
 }
 
 int main(void)
 {
-    int sum;
-
-    sum = sum_them_all(2, 98, 1024);
-    printf("%d\n", sum);
-    sum = sum_them_all(4, 98, 1024, 402, -1024);
-    printf("%d\n", sum);    
+    print_numbers(", ", 4, 0, 98, -1024, 402);
     return (0);
 }
